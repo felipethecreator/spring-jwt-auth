49b9bf1e7c4ad6242ce6ce8273725bed324b4016
felipethecreator <feliperodriguesqueiroz122@gmail.com>
2025-08-07 14:33:40 -0300

    feat(repository): create all repository files

---
+++ src/main/java/com/felipe/springjwtauth/repository/PostRepository.java
@@ -0,0 +1,9 @@
+package com.felipe.springjwtauth.repository;
+
+import com.felipe.springjwtauth.models.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, Long> {
+}
---
+++ src/main/java/com/felipe/springjwtauth/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.felipe.springjwtauth.repository;
+
+import com.felipe.springjwtauth.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+}
