a991ff97ee3fd6aebf272cd5f903ce653e2ea06b
felipethecreator <feliperodriguesqueiroz122@gmail.com>
2025-08-07 14:28:56 -0300

    feat(models): model Post, Role and User created

---
+++ src/main/java/com/felipe/springjwtauth/models/Post.java
@@ -0,0 +1,56 @@
+package com.felipe.springjwtauth.models;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.Instant;
+
+@Entity
+@Table(name = "posts")
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "post_id")
+    private Long postId;
+
+    @ManyToOne
+    private User user;
+
+    private String content;
+
+    @CreationTimestamp
+    private Instant creationTimeStamp;
+
+    public Long getPostId() {
+        return postId;
+    }
+
+    public void setPostId(Long postId) {
+        this.postId = postId;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Instant getCreationTimeStamp() {
+        return creationTimeStamp;
+    }
+
+    public void setCreationTimeStamp(Instant creationTimeStamp) {
+        this.creationTimeStamp = creationTimeStamp;
+    }
+}
---
+++ src/main/java/com/felipe/springjwtauth/models/Role.java
@@ -0,0 +1,31 @@
+package com.felipe.springjwtauth.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "role_id")
+    private long roleId;
+
+    private String name;
+
+    public long getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(long roleId) {
+        this.roleId = roleId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
